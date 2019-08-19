fcdd584cd7a314c6185aa2be2bbe3c7b92734ff8
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-18 18:29:37 -0700

    I had to refactor and delete some components in order to transition my css hover effect on the lgogos to be a more reliable pose-react animation

--- src/components/sidebar-components/OutsideLogoShell.js
+++ src/components/sidebar-components/OutsideLogoShell.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import OutsideLogo from './outsideLogo';
 import styles from './OutsideLogoShell.css';
 import githubLogo from '../../assets/githubLogo.png';
 import twitterLogo from '../../assets/twitterLogo.png';
 import linkedinLogo from '../../assets/linkedinLogo.png';
+import posed from 'react-pose';
+
+const OutsideLogo = posed.img({
+  hoverable: true,
+  pressable: true,
+  init: {
+    scale: 1.2,
+    background: '#afeeee'
+  },
+  hover: {
+    scale: 1.6,
+    background: '#f5f5f5'
+  },
+  press: {
+    scale: 1.4,
+    background: '#FFCF9E'
+  }
+});
 
 export default function OutsideLogoShell() {
   return (
-    <section className={styles.shell}>
-      <OutsideLogo 
-        linkTo='https://www.linkedin.com/in/alex-ramirez-de-cruz/' 
-        logoType={linkedinLogo} 
-      />
-      <OutsideLogo 
-        linkTo='https://github.com/aramirezdecruz3148' 
-        logoType={githubLogo} 
-      />
-      <OutsideLogo 
-        linkTo='https://twitter.com/dev_de_cruz' 
-        logoType={twitterLogo} 
-      />
-    </section>
+    <div className={styles.shell}>
+      <a href='https://www.linkedin.com/in/alex-ramirez-de-cruz/'>
+        <OutsideLogo className={styles.outsideLogo} src={linkedinLogo} />
+      </a>
+      <a href='https://github.com/aramirezdecruz3148'>
+        <OutsideLogo className={styles.outsideLogo} src={githubLogo} />
+      </a>
+      <a href='https://twitter.com/dev_de_cruz'>
+        <OutsideLogo className={styles.outsideLogo} src={twitterLogo} />
+      </a>
+    </div>
   );
 }
