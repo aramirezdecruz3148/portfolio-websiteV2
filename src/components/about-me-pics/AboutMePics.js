304d65ba4fadd70894685ddf1f88389ad7f61ecc
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-20 10:54:12 -0700

    started the beginnings of my AboutMePics component

---
+++ src/components/about-me-pics/AboutMePics.js
@@ -0,0 +1,16 @@
+//this will probably want to be a functional reusable
+//component that takes a src, text
+//will need to style the text ontop of the photo
+//look at Moddo for a reference 
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function AboutMePics() {
+  return ()
+}
+
+AboutMePics.propTypes = {
+
+};
+
+export default AboutMePics;
