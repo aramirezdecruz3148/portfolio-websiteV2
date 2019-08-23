db93a76f961ae79267208b5db8fd0a7e376e84e6
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-23 16:42:58 -0700

    I used a state hook within the OnLoad component to make the component display none when the animation completes, that way it doesn't block the clickability of the links

--- src/components/on-load img/OnLoad.js
+++ src/components/on-load img/OnLoad.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './OnLoad.css';
 
 function OnLoad({ imageSrc }) {
+  const [display, updateDisplay] = useState(true);
+
   return (
-    <div className={styles.onLoad}>
+    <div className={styles.onLoad} style={{ display: display ? 'block' : 'none' }} onAnimationEnd={() => updateDisplay(false)}>
       <img className={styles.img} src={imageSrc} />
     </div>
   );
