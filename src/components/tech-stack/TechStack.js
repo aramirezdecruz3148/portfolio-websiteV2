cdfa7544a24e32a9f5bb72345d2b8fd7948a8bfd
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-24 14:30:23 -0700

    I refactored my text stack achieving the look I originally wanted, working on css a bit

--- src/components/tech-stack/TechStack.js
+++ src/components/tech-stack/TechStack.js
@@ -1,69 +1,72 @@
 import React from 'react';
+import TreeView from '@material-ui/lab/TreeView';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import TreeItem from '@material-ui/lab/TreeItem';
 import styles from './TechStack.css';
-import { Card, CardContent } from '@material-ui/core';
 
-export default function TechStack() {
+export default function TestTechStack() {
   return (
     <div className={styles.techStack}>
-      <Card className={styles.card} raised={true} square={true}>
-        <CardContent className={styles.cardContent}>
-          <section>
-            <h4>LANGUAGES</h4>
-            <p>JavaScript</p>
-            <p>HTML</p>
-            <p>CSS</p>
-          </section>
-        </CardContent>
-      </Card>
-      <Card className={styles.card} raised={true} square={true}>
-        <CardContent className={styles.cardContent}>
-          <section>
-            <h4>FRONT-END</h4>
-            <p>React</p>
-            <p>Redux</p>
-          </section>
-        </CardContent>
-      </Card>
-      <Card className={styles.card} raised={true} square={true}>
-        <CardContent className={styles.cardContent}>
-          <section>
-            <h4>BACK-END</h4>
-            <p>Node.js</p>
-            <p>Express</p>
-          </section>
-        </CardContent>
-      </Card>
-      <Card className={styles.card} raised={true} square={true}>
-        <CardContent className={styles.cardContent}>
-          <section className={styles.section}>
-            <h4>DATABASES</h4>
-            <p>Firebase</p>
-            <p>MongoDB</p>
-            <p>Mongoose</p>
-          </section>
-        </CardContent>
-      </Card>
-      <Card className={styles.card} raised={true} square={true}>
-        <CardContent className={styles.cardContent}>
-          <section className={styles.section}>
-            <h4>TOOLS</h4>
-            <p>Github</p>
-            <p>Slack</p>
-            <p>VScode</p>
-            <p>Heroku</p>
-            <p>Netlify</p>
-          </section>
-        </CardContent>
-      </Card>
-      <Card className={styles.card} raised={true} square={true}>
-        <CardContent className={styles.cardContent}>
-          <section className={styles.section}>
-            <h4>TESTING</h4>
-            <p>Jest</p>
-            <p>QUnit</p>
-          </section>
-        </CardContent>
-      </Card>
+      <TreeView
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+      >
+        <TreeItem className={styles.title} nodeId="1" label="Languages">
+          <TreeItem className={styles.body} nodeId="2" label="JavaScript" />
+          <TreeItem className={styles.body} nodeId="3" label="HTML" />
+          <TreeItem className={styles.body} nodeId="4" label="CSS" />
+        </TreeItem>
+      </TreeView>
+      <TreeView
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+      >
+        <TreeItem nodeId="5" label="Front-End">
+          <TreeItem nodeId="6" label="React" />
+          <TreeItem nodeId="7" label="Redux" />
+        </TreeItem>
+      </TreeView>
+      <TreeView
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+      >
+        <TreeItem nodeId="8" label="Back-End">
+          <TreeItem nodeId="9" label="Node.js" />
+          <TreeItem nodeId="10" label="Express" />
+        </TreeItem>
+      </TreeView>
+      <TreeView
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+      >
+        <TreeItem nodeId="11" label="Databases">
+          <TreeItem nodeId="12" label="Firebase" />
+          <TreeItem nodeId="13" label="MongoDB" />
+          <TreeItem nodeId="14" label="Mongoose" />
+        </TreeItem>
+      </TreeView>
+      <TreeView
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+      >
+        <TreeItem nodeId="15" label="Tools">
+          <TreeItem nodeId="16" label="Github" />
+          <TreeItem nodeId="17" label="Slack" />
+          <TreeItem nodeId="18" label="VScode" />
+          <TreeItem nodeId="19" label="Heroku" />
+          <TreeItem nodeId="20" label="Netlify" />
+        </TreeItem>
+      </TreeView>
+      <TreeView
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+      >
+        <TreeItem nodeId="21" label="Testing">
+          <TreeItem nodeId="22" label="Jest" />
+          <TreeItem nodeId="23" label="QUnit" />
+        </TreeItem>
+      </TreeView>
     </div>
   );
 }
