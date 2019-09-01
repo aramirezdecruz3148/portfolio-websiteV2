b49890287c077c095e86e65c6985ea09a03b45b9
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-01 12:14:17 -0700

    continuing to customize the structrue of this first card to enable plugging the other two in. About to begin styling

--- src/components/portfolio/PortfolioCards.js
+++ src/components/portfolio/PortfolioCards.js
@@ -42,6 +42,7 @@ export default function PortfolioCards() {
 
   return (
     <Card
+      square={true}
       className={classes.card}
       raised={true}
       onMouseLeave={() => onHovered(false)}
@@ -58,14 +59,14 @@ export default function PortfolioCards() {
       />
       <CardContent>
         <p>
-          A directory containing all characters and spells within the Harry
+          <em>A directory containing all characters and spells within the Harry
           Potter universe. Click below to view the deployed app or click the expand icon for
-          project details and tech.
+          project details and tech.</em>
         </p>
       </CardContent>
       <CardActions>
         <a href='https://harry-potter-directory.netlify.com'>
-          <button>Deployed App</button>
+          <button>View App</button>
         </a>
         <IconButton
           className={clsx(classes.expand, {
@@ -81,10 +82,23 @@ export default function PortfolioCards() {
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
           <section>
-            <p>Description of the project, as well as my contributions.</p>
+            <p>I created this project as a directory and resource for all characters 
+              and spells within the Harry Potter universe. The character directory provides
+              details on each characters school, house, and blood status as well as the
+              ability to filter all characters based on house, blood status and group
+              affiliation (D.A, Death Eater, etc). The spell directory provides a list
+              of all spells in alphabetical order that appear within the 7 books of the
+              Harry Potter series. For each spell you are given the type (charm, hex, curse, etc)
+              and the effect of the spell.
+            </p>
             <p>
               <strong>Tech:</strong>
             </p>
+            <ul>
+              <li>JavaScript components</li>
+              <li>HTML/CSS</li>
+              <li>Harry Potter API: https://www.potterapi.com/</li>
+            </ul>
           </section>
         </CardContent>
       </Collapse>
