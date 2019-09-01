c6e805384a6f0e7c622bda445498eaaa573defc6
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-01 15:53:28 -0700

    I got the card mostly styled

--- src/components/portfolio/PortfolioCards.js
+++ src/components/portfolio/PortfolioCards.js
@@ -11,14 +11,15 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import styles from './PortfolioCards.css';
 import bwHP from '../../assets/bw-HP.png';
 import HP from '../../assets/HP.png';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   card: {
-    maxWidth: 345
+    maxWidth: '22vw'
   },
   media: {
-    height: 0,
-    paddingTop: '56.25%' // 16:9
+    height: '0',
+    paddingTop: '12vw'
   },
   expand: {
     transform: 'rotate(0deg)',
@@ -29,11 +30,34 @@ const useStyles = makeStyles(theme => ({
   },
   expandOpen: {
     transform: 'rotate(180deg)'
+  },
+  root: {
+    fontSize: '2.2vw',
+    padding: '0'
+  }
+}));
+
+const adjustSpacing = makeStyles(() => ({
+  root: {
+    padding: '1vw'
+  },
+  body1: {
+    fontFamily: 'Assistant',
+    fontSize: '1vw',
+    margin: '0'
+  },
+  h5: {
+    fontFamily: 'Roboto Condensed',
+    fontSize: '1.2vw',
+    fontWeight: 'bolder',
+    marginTop: '1vw',
+    textAlign: 'left'
   }
 }));
 
 export default function PortfolioCards() {
   const classes = useStyles();
+  const spacing = adjustSpacing();
   const [expanded, setExpanded] = React.useState(false);
   const [hover, onHovered] = React.useState(false);
 
@@ -50,9 +74,9 @@ export default function PortfolioCards() {
       onMouseEnter={() => onHovered(true)}
       onClick={() => onHovered(true)}
     >
-      <CardContent className={styles.card}>
-        <h4>HARRY POTTER DIRECTORY</h4>
-        <h5>FRONT-END APP</h5>
+      <CardContent spacing={spacing.root} className={styles.card}>
+        <Typography variant='h4'>HARRY POTTER DIRECTORY</Typography>
+        <Typography variant='h5'>FRONT-END APP</Typography>
       </CardContent>
       <CardMedia
         className={classes.media}
@@ -60,15 +84,17 @@ export default function PortfolioCards() {
         title="screen shot of my HP site"
       />
       <CardContent>
-        <p className={styles.caption}>
-          <em>A directory containing all characters and spells within the Harry
-          Potter universe. Click below to view the deployed app or click the expand icon for
-          project details and tech.</em>
-        </p>
+        <p className={styles.caption}> A directory containing all characters and spells within the Harry
+            Potter universe. Click below to view the deployed app or click the
+            expand icon for project details and tech.</p>
       </CardContent>
-      <CardActions>
-        <a href='https://harry-potter-directory.netlify.com'>
-          <button className={styles.anchorButtons}>View<br />App</button>
+      <CardActions className={spacing.root}>
+        <a href="https://harry-potter-directory.netlify.com">
+          <button className={styles.anchorButtons}>
+            VIEW
+            <br />
+            APP
+          </button>
         </a>
         <IconButton
           className={clsx(classes.expand, {
@@ -78,30 +104,26 @@ export default function PortfolioCards() {
           aria-expanded={expanded}
           aria-label="show more"
         >
-          <ExpandMoreIcon />
+          <ExpandMoreIcon className={classes.root} />
         </IconButton>
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <section>
-            <p>I created this project as a directory and resource for all characters 
-              and spells within the Harry Potter universe. The character directory provides
-              details on each characters school, house, and blood status as well as the
-              ability to filter all characters based on house, blood status and group
-              affiliation (D.A, Death Eater, etc). The spell directory provides a list
-              of all spells in alphabetical order that appear within the 7 books of the
-              Harry Potter series. For each spell you are given the type (charm, hex, curse, etc)
-              and the effect of the spell.
-            </p>
-            <p>
-              <strong>Tech:</strong>
-            </p>
-            <ul>
-              <li>JavaScript components</li>
-              <li>HTML/CSS</li>
-              <li>Harry Potter API: https://www.potterapi.com/</li>
-            </ul>
-          </section>
+          <Typography paragraph className={spacing.body1}>
+          I created this project as a directory and resource for all
+            characters and spells within the Harry Potter universe. The
+            character directory provides details on each characters school,
+            house, and blood status as well as the ability to filter all
+            characters based on house, blood status and group affiliation (D.A,
+            Death Eater, etc). The spell directory provides a list of all spells
+            in alphabetical order that appear within the 7 books of the Harry
+            Potter series. For each spell you are given the type (charm, hex,
+            curse, etc) and the effect of the spell.
+          </Typography>
+          <Typography className={spacing.h5} variant='h5'>TECH</Typography>
+          <Typography paragraph variant='body1' className={spacing.body1}>• JavaScript Components</Typography>
+          <Typography paragraph variant='body1' className={spacing.body1}>• HTML/CSS</Typography>
+          <Typography paragraph variant='body1' className={spacing.body1}>• Harry Potter API</Typography>
         </CardContent>
       </Collapse>
     </Card>
