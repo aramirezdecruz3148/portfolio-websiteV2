2081cd7f8bd345c24f5aeb80ebd0840cc48b7b8d
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-02 15:13:51 -0700

    was able to successfully rendered the Home view using Browser router, going to move onto testing making a success page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import styles from './App.css';
-import Sidebar from '../containers/sidebar/Sidebar';
-import MainContent from '../containers/main-content/MainContent';
-import myLogo from '../../assets/myLogo.png';
-import OnLoad from '../on-load img/OnLoad';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from '../pages/home/Home.js';
+
 
 export default class App extends Component {
   render() {
     return (
       <>
-        <OnLoad imageSrc={myLogo} />
-        <div className={styles.app}>
-          <Sidebar />
-          <MainContent />
-        </div>
+        <Router>
+          <Route exact path='/' component={Home} />
+        </Router>
       </>
     );
   }
