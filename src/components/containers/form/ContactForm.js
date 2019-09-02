5c2a239ddbbc03831af2c17b248bf443baa95bc7
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-02 14:37:13 -0700

    okay, I lied, now I am done styling the form

--- src/components/containers/form/ContactForm.js
+++ src/components/containers/form/ContactForm.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Form from '../../contact-form/Form';
+import styles from './ContactForm.css';
 
 export default function ContactForm() {
   return (
-    <>
+    <div className={styles.space}>
+      <h2 className={styles.title}>WANT TO COLLABORATE? SEND ME A MESSAGE!</h2>
       <Form />
-    </>
+    </div>
   );
 }
