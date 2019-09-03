8ab642a5044bbd63e97c2146b6f1002d83c64376
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-02 18:05:48 -0700

    I was able to figure out putting the portfolio into a slideshow view, now need to style buttons and adjust portfolio css

--- src/components/portfolio/Wrapper.js
+++ src/components/portfolio/Wrapper.js
@@ -1,20 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Wrapper.css';
 import Hp from './HP';
 import Bot from './Bot';
 import Moddo from './Moddo';
 
+const portfolio = [Hp, Bot, Moddo];
+
 export default function Wrapper() {
+  const [countObj, setCount] = useState({
+    count: 0,
+    next: portfolio.length <= 1,
+    previous: true
+  });
+
+  const incrementNewCount = countObj.count + 1;
+  const decrementNewCount = countObj.count - 1;
+
+  const updateState = updatedCount => {
+    setCount({
+      count: updatedCount,
+      next: updatedCount >= portfolio.length - 1,
+      previous: updatedCount === 0
+    });
+  };
+
+  if(countObj.count === 1) {
+    return (
+      <div className={styles.top}>
+        <div className={styles.center}>
+          <h2 className={styles.title}>PORTFOLIO</h2>
+        </div>
+        <button
+          disabled={countObj.previous}
+          onClick={() => updateState(decrementNewCount)}
+        >
+          decrease
+        </button>
+        <div className={styles.wrapper}>
+          <Bot />
+        </div>
+        <button
+          disabled={countObj.next}
+          onClick={() => updateState(incrementNewCount)}
+        >
+          increase
+        </button>
+      </div>
+    );
+  } else if(countObj.count === 2) {
+    return (
+      <div className={styles.top}>
+        <div className={styles.center}>
+          <h2 className={styles.title}>PORTFOLIO</h2>
+        </div>
+        <button
+          disabled={countObj.previous}
+          onClick={() => updateState(decrementNewCount)}
+        >
+          decrease
+        </button>
+        <div className={styles.wrapper}>
+          <Moddo />
+        </div>
+        <button
+          disabled={countObj.next}
+          onClick={() => updateState(incrementNewCount)}
+        >
+          increase
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.top}>
       <div className={styles.center}>
         <h2 className={styles.title}>PORTFOLIO</h2>
       </div>
+      <button
+        disabled={countObj.previous}
+        onClick={() => updateState(decrementNewCount)}
+      >
+        decrease
+      </button>
       <div className={styles.wrapper}>
         <Hp />
-        <Bot />
-        <Moddo />
       </div>
+      <button
+        disabled={countObj.next}
+        onClick={() => updateState(incrementNewCount)}
+      >
+        increase
+      </button>
     </div>
   );
 }
