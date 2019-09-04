c29d371e06b5c28c0b0c31b6303ec0c76a437327
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-03 20:52:30 -0700

    trying something different to avoid having to convert some of what I have made into HTML

---
+++ public/components/SuccessApp.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function SuccessApp() {
+  return (
+    <h1>YO</h1>
+  );
+}
---
+++ public/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import SuccessApp from './components/SuccessApp';
+
+render(
+  <SuccessApp />,
+  document.getElementById('root')
+);
