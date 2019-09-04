ef94cc746f2594f23fb667a9cdfad1a2b2d72a98
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-03 20:38:45 -0700

    put the route back

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from '../pages/home/Home.js';
+import Success from '../pages/success/Success.js';
 
 export default class App extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class App extends Component {
       <>
         <Router>
           <Route exact path='/' component={Home} />
+          <Route path='/success' component={Success} />
         </Router>
       </>
     );
