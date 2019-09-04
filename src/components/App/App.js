d0d2e6fda24b7be11e26146741454e2753566000
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-04 08:11:00 -0700

    it looks like the redirect works without app route, but I am also not pointing to the redirect, so I am not sure how it knows, but I put the action back in both forms, just wanna test one more time

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from '../pages/home/Home.js';
-import Success from '../pages/success/Success.js';
 
 export default class App extends Component {
   render() {
