4749323f78e9e4aac59fa07ba2c24b687b6c2d2e
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-04 08:30:50 -0700

    didn't realize that app still had the success route on this brach, got rid of that and ammended the snapshot to avoid merge conflicts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from '../pages/home/Home.js';
-import Success from '../pages/success/Success.js';
 
 export default class App extends Component {
   render() {
@@ -9,7 +8,6 @@ export default class App extends Component {
       <>
         <Router>
           <Route exact path='/' component={Home} />
-          <Route path='/success' component={Success} />
         </Router>
       </>
     );
