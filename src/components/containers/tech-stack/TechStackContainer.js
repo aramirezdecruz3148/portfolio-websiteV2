fd7bd3bff9818718336cabf9894ad07f3d2a8a04
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-05 13:12:34 -0700

    finally figured out how to reach and override the font size of the tech stack tree view

--- src/components/containers/tech-stack/TechStackContainer.js
+++ src/components/containers/tech-stack/TechStackContainer.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import styles from './TechStackContainer.css';
 import TechStack from '../../tech-stack/TechStack';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const defaultTheme = createMuiTheme();
+const theme = createMuiTheme({
+  typography: {
+    body1: {
+      fontSize: '1.4vw',
+    }
+  },
+});
 
 export default function TechStackContainer() {
   return (
-    <div>
-      <h2 className={styles.title}>TECH STACK</h2>
-      <TechStack />
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <div>
+        <h2 className={styles.title}>TECH STACK</h2>
+        <ThemeProvider theme={theme}>
+          <TechStack />
+        </ThemeProvider>
+      </div>
+    </ThemeProvider>
   );
 }
