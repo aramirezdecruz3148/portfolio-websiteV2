feacdcdd2758aae5e20ff3e8ffa6ff73ffa457d6
aramirezdecruz3148 <leigh3148@gmail.com>
2020-08-29 19:55:21 -0700

    Added new tech to my stack

--- src/components/tech-stack/TechStack.js
+++ src/components/tech-stack/TechStack.js
@@ -25,9 +25,10 @@ export default function TestTechStack() {
           defaultExpandIcon={<ChevronRightIcon className={classes.root} />}
         >
           <TreeItem nodeId='1' label='Languages'>
-            <TreeItem nodeId='2' label='JavaScript' />
-            <TreeItem nodeId='3' label='HTML' />
-            <TreeItem nodeId='4' label='CSS' />
+            <TreeItem nodeId='2' label='Ruby' />
+            <TreeItem nodeId='3' label='JavaScript' />
+            <TreeItem nodeId='4' label='HTML' />
+            <TreeItem nodeId='5' label='CSS' />
           </TreeItem>
         </TreeView>
       </Typography>
@@ -36,9 +37,10 @@ export default function TestTechStack() {
           defaultCollapseIcon={<ExpandMoreIcon className={classes.root} />}
           defaultExpandIcon={<ChevronRightIcon className={classes.root} />}
         >
-          <TreeItem nodeId='5' label='Front-End'>
-            <TreeItem nodeId='6' label='React' />
-            <TreeItem nodeId='7' label='Redux' />
+          <TreeItem nodeId='6' label='Front-End'>
+            <TreeItem nodeId='7' label='React' />
+            <TreeItem nodeId='8' label='Redux' />
+            <TreeItem nodeId='9' label='jQuery' />
           </TreeItem>
         </TreeView>
       </Typography>
@@ -47,9 +49,10 @@ export default function TestTechStack() {
           defaultCollapseIcon={<ExpandMoreIcon className={classes.root} />}
           defaultExpandIcon={<ChevronRightIcon className={classes.root} />}
         >
-          <TreeItem nodeId='8' label='Back-End'>
-            <TreeItem nodeId='9' label='Node.js' />
-            <TreeItem nodeId='10' label='Express' />
+          <TreeItem nodeId='10' label='Back-End'>
+            <TreeItem nodeId='11' label='Ruby' />
+            <TreeItem nodeId='12' label='Node.js' />
+            <TreeItem nodeId='13' label='Express' />
           </TreeItem>
         </TreeView>
       </Typography>
@@ -58,10 +61,11 @@ export default function TestTechStack() {
           defaultCollapseIcon={<ExpandMoreIcon className={classes.root} />}
           defaultExpandIcon={<ChevronRightIcon className={classes.root} />}
         >
-          <TreeItem nodeId='11' label='Databases'>
-            <TreeItem nodeId='12' label='Firebase' />
-            <TreeItem nodeId='13' label='MongoDB' />
-            <TreeItem nodeId='14' label='Mongoose' />
+          <TreeItem nodeId='14' label='Databases'>
+            <TreeItem nodeId='15' label='PostgreSQL' />
+            <TreeItem nodeId='16' label='Firebase' />
+            <TreeItem nodeId='17' label='MongoDB' />
+            <TreeItem nodeId='18' label='Mongoose' />
           </TreeItem>
         </TreeView>
       </Typography>
@@ -70,12 +74,13 @@ export default function TestTechStack() {
           defaultCollapseIcon={<ExpandMoreIcon className={classes.root} />}
           defaultExpandIcon={<ChevronRightIcon className={classes.root} />}
         >
-          <TreeItem nodeId='15' label='Tools'>
-            <TreeItem nodeId='16' label='Github' />
-            <TreeItem nodeId='17' label='Slack' />
-            <TreeItem nodeId='18' label='VScode' />
-            <TreeItem nodeId='19' label='Heroku' />
-            <TreeItem nodeId='20' label='Netlify' />
+          <TreeItem nodeId='19' label='Tools'>
+            <TreeItem nodeId='20' label='Github' />
+            <TreeItem nodeId='21' label='Slack' />
+            <TreeItem nodeId='22' label='Jira' />
+            <TreeItem nodeId='23' label='VScode' />
+            <TreeItem nodeId='24' label='Heroku' />
+            <TreeItem nodeId='25' label='Netlify' />
           </TreeItem>
         </TreeView>
       </Typography>
@@ -84,9 +89,12 @@ export default function TestTechStack() {
           defaultCollapseIcon={<ExpandMoreIcon className={classes.root} />}
           defaultExpandIcon={<ChevronRightIcon className={classes.root} />}
         >
-          <TreeItem nodeId='21' label='Testing'>
-            <TreeItem nodeId='22' label='Jest' />
-            <TreeItem nodeId='23' label='QUnit' />
+          <TreeItem nodeId='26' label='Testing'>
+            <TreeItem nodeId='27' label='Cypress' />
+            <TreeItem nodeId='28' label='Rspec' />
+            <TreeItem nodeId='29' label='Semaphore CI' />
+            <TreeItem nodeId='30' label='Jest' />
+            <TreeItem nodeId='31' label='QUnit' />
           </TreeItem>
         </TreeView>
       </Typography>
