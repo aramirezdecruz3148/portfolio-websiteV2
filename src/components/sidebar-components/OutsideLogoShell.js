cde19a99080da94147dc60ea76c89df8b2d57ee4
Alex Ramirez de Cruz <aramirezdecruz@gmail.com>
2022-11-18 08:50:27 -0800

    removing twitter

--- src/components/sidebar-components/OutsideLogoShell.js
+++ src/components/sidebar-components/OutsideLogoShell.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './OutsideLogoShell.css';
 import githubLogo from '../../assets/githubLogo.png';
-import twitterLogo from '../../assets/twitterLogo.png';
 import linkedinLogo from '../../assets/linkedinLogo.png';
 import posed from 'react-pose';
 
@@ -22,23 +21,6 @@ const OutsideLogo = posed.img({
   }
 });
 
-const TwitterLogo = posed.img({
-  hoverable: true,
-  pressable: true,
-  init: {
-    scale: 1.2,
-    background: '#afeeee'
-  },
-  hover: {
-    scale: 1.6,
-    transition: 'all 1s ease',
-  },
-  press: {
-    scale: 1.4,
-    background: '#afeeee'
-  }
-});
-
 export default function OutsideLogoShell() {
   return (
     <div className={styles.shell}>
@@ -47,9 +29,6 @@ export default function OutsideLogoShell() {
       </a>
       <a href='https://github.com/aramirezdecruz3148' rel='noopener noreferrer' target='_blank'>
         <OutsideLogo className={styles.outsideLogo} src={githubLogo} />
-      </a>
-      <a href='https://twitter.com/dev_de_cruz' rel='noopener noreferrer' target='_blank'>
-        <TwitterLogo className={styles.twitterLogo} src={twitterLogo} />
       </a>
     </div>
   );
